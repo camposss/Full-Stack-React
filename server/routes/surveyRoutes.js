1e6675d52fc8524acd0282d71dbae203c00141fb
camposss <ccamposs23@gmail.com>
2018-02-13 09:59:59 -0800

    wrote a query for the webhook endpoint, need to check if it works on mlab

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,18 +1,19 @@
 //our middleware is requireLogin; user must be logged in to do a survey
 //you want to put the middlewares in the order of execution... logged in first => make sure they have credits => then req/res
-
 //you have to require in mongoose at the top in order to avoid errors during testing. Mongoose doesn't like multiple files being imported without the require mongoose step at the top.
-
 // _user is available to use through the mongo/mongoose model
-
 //Webhook is when a 3rd party api gives us a response through a callback after something they did (Sendgrid sends us the message about the click to our servers)
 
+const _ =require('lodash');
+const Path = require('path-parser');
+const {URL}= require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 const Mailer = require('../services/mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
+
 module.exports = app =>{
     app.get('/api/surveys/thanks', (req,res)=>{
         res.send('thanks for voting!');
@@ -20,7 +21,30 @@ module.exports = app =>{
 
 
     app.post('/api/surveys/webhooks', (req,res)=>{
-        console.log(req.body);
+        const p= new Path('/api/surveys/:surveyId/:choice');
+        _.chain(req.body)
+            .map(({email,url}) =>{
+                //if it cant extract survey id and choice then it will return null thats why you cant destructure the match variable
+                const match = p.test(new URL(url).pathname);
+                if(match){
+                    return {email, surveyId: match.surveyId, choice: match.choice};
+                }
+            })
+            .compact()
+            .uniqBy('email', 'surveyId')
+            .each(({surveyId,email, choice})=>{
+                Survey.updateOne({
+                    _id: surveyId,
+                    recipients: {
+                        $elemMatch:{email: email, responded: false}
+                    }
+                },{
+                    $inc: { [choice]:1 },
+                    $set: { 'recipients.$.responded': true }
+
+                }).exec();
+            })
+            .value();
         res.send({});
     });
     app.post('/api/surveys', requireLogin, requireCredits, async (req,res)=>{
--- server/services/test.js
+++ server/services/test.js
@@ -1,8 +1,6 @@
 //write a function to retrieve a blob of json
 //make an ajax request using fetch function
-
 // http://rallycoding.herokuapp.com/api/music_albums
-
 // function fetchAlbums(){
 //     fetch('https://rallycoding.herokuapp.com/api/music_albums')
 //         .then(res=> res.json())
@@ -16,3 +14,21 @@
 }
 
 fetchAlbums();
+
+
+//go in and find this and update the object
+//the second object we pass is the data we want to send back and update the database
+//$inc mongo operator, helps us write slightly intelligent logic into the query; it means to increment either yes or no
+// [choice] what's the value of the choice variable, we don't know at the time writing the code
+//$ set one of the appropriate recipients in the subdocument collection to true
+//this skips the process of fetching all of the data and going one by one. Mongo takes this, finds the match and updates it all in one
+Survey.updateOne({
+    id: surveyId,
+    recipients: {
+        $elemMatch:{email: email, responded: false}
+    }
+},{
+    $inc: { [choice]:1 },
+    $set: { 'recipients.$.responded': true }
+
+});
